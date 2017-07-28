c4a8a3600f6ab675f5efe0125156163edb085504
Richard Mumford <rmumford@esriuk.com>
2017-07-28 12:04:05 +0100

    Added a Utils module to handle conversions and stuff

--- js/mapstyler.js
+++ js/mapstyler.js
@@ -1,8 +1,18 @@
  require([
         "modules/MapController",
         "modules/Palette",
-        "modules/Portal"
-    ], function(MapController, Palette, Portal) {
+        "modules/Portal",
+        "modules/Utils"
+    ], function(MapController, Palette, Portal, Utils) {
         var mapController = new MapController.MapController("viewDiv");
-        mapController.buildMap();
+        var paletteCollection = new Array; //where 0 is the latest and the rest are for the undo stack
+
+        function initialise(){
+            paletteCollection[0] = new Palette();
+            mapController.buildMap().done(function () { 
+                //mapController.applyPalette();
+            });
+
+        }
+        
     });
---
+++ js/modules/Utils.js
@@ -0,0 +1,12 @@
+define([], function() {
+
+    var imageToBase64 = function(){
+        
+    }
+
+    //Stuff to make public
+    return {
+        imageToBase64: imageToBase64
+    };
+
+});
