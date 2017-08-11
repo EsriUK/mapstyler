1402b7d69d10a4c8e50e7c1d3eeff23ec38a68ae
Richard Mumford <rmumford@esriuk.com>
2017-08-11 10:33:41 +0100

    Store the style in a palette

--- js/modules/Portal.js
+++ js/modules/Portal.js
@@ -75,7 +75,7 @@ define([
             //$('.sk-circle').show();
             portal.authMode = "immediate";
             portal.load().then(function () {
-                $('#signout').show();
+                //$('#signout').show();
                 var time = new Date();
                 var year = time.getFullYear();
                 var month = time.getMonth() + 1;
